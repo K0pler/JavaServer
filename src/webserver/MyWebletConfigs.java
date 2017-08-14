b8bfd2e065885180a43f8597c8018fd045edc848
Martin Pedersen <martinpedersen65@gmail.com>
2017-08-14 14:37:15 +0200

    Added a serverstartup interface

--- src/webserver/MyWebletConfigs.java
+++ src/webserver/MyWebletConfigs.java
@@ -20,4 +20,8 @@ public class MyWebletConfigs {
 			new MyWebletConfigs ("/HelloWorld", apps.HelloWorldMyWeblet.class),
 			new MyWebletConfigs ("/ProcessName", apps.NameProcessor.class),
 	};
+	
+	static String[] serverStartupClasses = {
+			"apps.MyServerStartup"
+	};
 }
--- src/webserver/Server.java
+++ src/webserver/Server.java
@@ -9,12 +9,27 @@ public class Server {
 		@SuppressWarnings("resource")
 		ServerSocket server = new ServerSocket(1024);
 		
+		for (String className: MyWebletConfigs.serverStartupClasses) {
+			
+			try {
+				@SuppressWarnings("rawtypes")
+				Class cls = Class.forName(className);
+				ServerStartup serverStartupObject = (ServerStartup) cls.newInstance();
+				// Call the startup method
+				serverStartupObject.onServerStartup();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		
 		for (;;) {
 			System.out.println("Waiting for client connection");
             Socket socket = server.accept();
             System.out.println("Have client connection, launching thread");
 			new ServerInstance(socket).start();
 		}
+		
+		
 
 	}
 
